d5bd252744c7e5b25584be63efdc5d930f7a8216
jonatas.santos <jonatas.santos@teladoc.com>
2021-02-27 21:46:49 -0300

    Done flutter calculator

--- lib/model/memory.dart
+++ lib/model/memory.dart
@@ -1,15 +1,93 @@
 class Memory {
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  String _operation;
   String _value = '0';
+  bool _wipeValue = false;
+  String _lastCommand;
+  String get value => _value;
 
   void applyCommand(String command) {
-    command == 'AC' ? _allClear() : _value += command;
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
+    if (command == 'AC') {
+      _allClear();
+    } else if (operations.contains(command)) {
+      _setOperation(command);
+    } else {
+      _addDigit(command);
+    }
+    _lastCommand = command;
+  }
+
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        _lastCommand != '=' &&
+        command != '=';
+  }
+
+  _setOperation(String newOpeation) {
+    bool isEqualSign = newOpeation == '=';
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOpeation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+      _operation = newOpeation;
+      _bufferIndex = 1;
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? null : newOpeation;
+      _bufferIndex = isEqualSign ? 0 : 1;
+    }
+    _wipeValue = true;
+  }
+
+  _addDigit(String digit) {
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if (isDot && _value.contains('.') && !wipeValue) return;
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? emptyValue : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = null;
+    _wipeValue = false;
   }
 
-  String get value {
-    return _value;
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 }
--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -2,6 +2,7 @@ import 'package:calculator_flutter/components/display.dart';
 import 'package:calculator_flutter/components/keyboard.dart';
 import 'package:calculator_flutter/model/memory.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Calculator extends StatefulWidget {
   @override
@@ -19,6 +20,8 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       home: Column(
         children: [
